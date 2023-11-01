7ab4ec36f8c48d3ce42ae6c952eeeb6856f6bdc1
Pankaj79090 <144835934+Pankaj79090@users.noreply.github.com>
2023-11-01 17:41:37 +0530

    Add files via upload

---
+++ lab2.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+int main(){
+	int a,b,c,d,e,g;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	c=a*b;
+	d=a+b;
+	e=a-b;
+	g=a/b; 
+	printf("%d \n",c); 
+	printf("%d \n",d);
+	printf("%d \n",e);
+	printf("%d \n",g);
+	return 0;
+}
